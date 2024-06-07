676b83a62c1b044d2efc86af51941b80c867728d
Lee, Hyeon-gi <maaya.lee@gmail.com>
2024-06-07 18:04:37 +0900

    Change docker.io registry

--- nginx-cache/create.sh
+++ nginx-cache/create.sh
@@ -3,4 +3,4 @@ sudo docker rm maayalee-nginx-cache
 sudo docker run --name maayalee-nginx-cache -d \
   -v /home/glitchinfra1/html:/usr/share/nginx/html \
 	-p 80:80 \
-	localhost:5000/maayalee/nginx/cache
+	docker.io/maayalee/nginx/cache
