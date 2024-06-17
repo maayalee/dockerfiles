2b3f9f6de6ebd1e1ea2091ddd9f1a8f8d2d71180
Lee, Hyeon-gi <maaya.lee@gmail.com>
2024-06-17 18:23:14 +0800

    Delete host mode parameter

--- node_exporter/run.sh
+++ node_exporter/run.sh
@@ -3,7 +3,6 @@ sudo docker run -d \
   --name=node-exporter \
   --restart=unless-stopped \
   --publish=9100:9100 \
-  --net="host" \
   --pid="host" \
   -v "/:/host:ro,rslave" \
   maayalee/node_exporter:latest \
